030730b9caf207e1fa6d57a272170c9b368ce6d4
hhoao <haunghao@foxmail.com>
2026-04-22 22:00:27 +0800

    refactor(core): remove unused classes and utilities

--- kline-server/kline-core/src/main/java/com/hhoa/kline/core/core/shared/ExtensionMessageConstants.java
+++
@@ -1,7 +0,0 @@
-package com.hhoa.kline.core.core.shared;
-
-public class ExtensionMessageConstants {
-    public static final String COMMAND_CANCEL_TOKEN = "__cline_command_cancel__";
-
-    public static final String COMPLETION_RESULT_CHANGES_FLAG = "HAS_CHANGES";
-}
--- kline-server/kline-core/src/main/java/com/hhoa/kline/core/core/task/DefaultContext.java
+++
@@ -1,9 +0,0 @@
-package com.hhoa.kline.core.core.task;
-
-/**
- * DefaultContext
- *
- * @author xianxing
- * @since 2026/2/12
- */
-public class DefaultContext {}
