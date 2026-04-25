0c88dacf1a9040e1e0d1c6ffaf3b4393c9e4fb8a
hhoao <haunghao@foxmail.com>
2026-04-25 23:08:31 +0800

    refactor: remove langchain4j module

--- kline-server/kline-langchain4j/src/main/java/com/hhoa/kline/langchain4j/StreamingChatCallback.java
+++
@@ -1,9 +0,0 @@
-package com.hhoa.kline.langchain4j;
-
-public interface StreamingChatCallback {
-    void onPartialResponse(String partialResponse);
-
-    void onCompleteResponse(Integer inputTokens, Integer outputTokens);
-
-    void onError(Throwable error);
-}
--- kline-server/kline-langchain4j/src/main/java/com/hhoa/kline/langchain4j/StreamingChatClient.java
+++
@@ -1,8 +0,0 @@
-package com.hhoa.kline.langchain4j;
-
-import dev.langchain4j.data.message.ChatMessage;
-import java.util.List;
-
-interface StreamingChatClient {
-    void stream(List<ChatMessage> messages, StreamingChatCallback callback);
-}
